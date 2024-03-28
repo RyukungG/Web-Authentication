06881b6ad9e41348087b88cff226facab897e5b3
RyukungG <ryu.nosuke@hotmail.com>
2024-03-29 00:37:03 +0700

    Finish Spring Security 4

--- src/main/java/ku/kinkao/entity/Member.java
+++ src/main/java/ku/kinkao/entity/Member.java
@@ -1,23 +1,25 @@
 package ku.kinkao.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Data;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
 
 @Data
 @Entity
 public class Member {
 
-
     @Id
     @GeneratedValue
-    private int id;
-
+    private UUID id;
 
     private String username;
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
+    private Instant createdAt;
 }
+
