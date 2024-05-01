b3ba8ee18387d7d9952bb0f3d754206149a49ab2
RyukungG <ryu.nosuke@hotmail.com>
2024-05-01 22:16:11 +0700

    Finish Spring Security 7

--- src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
+++ src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
@@ -3,6 +3,8 @@ package ku.kinkao.service;
 
 import ku.kinkao.entity.Member;
 import ku.kinkao.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,12 +13,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
+
+
+    Logger logger = LoggerFactory.getLogger(UserDetailsServiceImp.class);
 
 
     @Autowired
@@ -35,11 +40,16 @@ public class UserDetailsServiceImp implements UserDetailsService {
             throw new UsernameNotFoundException("Could not find user");
         }
 
+
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(member.getRole()));
 
+
+        logger.info(username + " has successfully logged in at "
+                + Instant.now() + ": logged in as " + member.getRole());
+
+
         return new org.springframework.security.core.userdetails.User(
-                member.getUsername(), member.getPassword(),
-                authorities);
+                member.getUsername(), member.getPassword(), authorities);
     }
 }
